913897f1084c014ed6df4ad4c89b834541eafe22
Mathilde Ferrera <mathilde.ferrera@kedgebs.com>
2019-04-08 18:22:59 +0200

    akinator kedge

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,35 @@
 class GamesController < ApplicationController
+
+QUESTIONS = [
+"A t'il les yeux clairs ?",
+"Est-il un homme ?",
+"Est-il vegan ?",
+"Est-il originaire d'aquitaine ?"
+]
+
+
   def create
-  	redirect_to edit_game_path(1), method: :get
+    #create a new game!
+    @game = Game.create
+  	redirect_to edit_game_path(@game["id"], question_id: 0), method: :get
   end
 
   def edit
-
+    @questions = QUESTIONS[params["question_id"].to_i]
   end
 
   def update
-	redirect_to game_path(1), method: :get
+	  question_id = params["question_id"].to_i + 1
+    @game = Game.find(params["id"])
+    @game["answers"] << params["answers"]
+    @game.save
+    if question_id > 3
+       redirect_to game_path(@game["id"]), method: :get
+    else 
+       redirect_to edit_game_path(@game["id"], question_id: question_id), method: :get
   end
 
   def show
+    @game = Game.find(params["id"])
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,30 @@ class PagesController < ApplicationController
 
   def blog
   	article1 = { 
-	"title" => "Mon 1er article",
-  	"content"=> "Lorem",
+	  "title" => "Objectif",
+  	"content"=> "Notre objectif est d'appliquer au domaine des NTIC 
+    (nouvelles technologies de l’information et de la communication) 
+    des solutions innovantes élaborées par notre laboratoire de recherche 
+    et développement en profitant des dernières avancées de la recherche.",
   	"rating" => 3
 	}
 
 	article2 = { 
-	"title" => "Mon 2e article",
-  	"content"=> "Lorem",
+	  "title" => "Projet futur",
+  	"content"=> "Dans le domaine du divertissement, notre génie Akinator 
+    est devenu une célebrité internationale du Web et du mobile et nous 
+    travaillons continuellement sur son amélioration. Mais nous ne comptons 
+    pas en rester là...",
   	"rating" => 3
   	}
 
 	article3 = { 
-	"title" => "Mon 3e article",
-  	"content"=> "Lorem",
+	  "title" => "Au service de l'hunain",
+  	"content"=> "Confrontés à une masse d'information de plus en plus collossale, 
+    les utilisateurs d'aujourd'hui ont besoin d'outils intelligents qui les aident 
+    à s'y retrouver, à obtenir facilement les réponses à leurs questions et à leurs 
+    attentes. Mathilde et Loona participent à l'élaboration de ces solutions du futur 
+    qui organiseront l'information en la mettant au service de l'humain.",
   	"rating" => 3
 	}
 
